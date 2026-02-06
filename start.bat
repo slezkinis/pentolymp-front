f6835d6859c4ad8835a18db2e7b83d245c6a4ed6
slezkinis <slezkin.is@mail.ru>
2026-02-06 20:57:35 +0300

    Remove dev server in front

--- start.bat
+++ start.bat
@@ -1,13 +1,19 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
-REM Установка значения по умолчанию если BACKEND_URL не задан
-if not defined BACKEND_URL (
-    set "BACKEND_URL=http://host.docker.internal:8000"
+REM Backend URL по умолчанию
+if "%BACKEND_URL%"=="" (
+  set BACKEND_URL=http://host.docker.internal:8000
 )
 
-REM Замена переменных в traefik-dynamic.yml перед запуском
-powershell -Command "(Get-Content traefik-dynamic.yml.template) -replace '\${BACKEND_URL}', '!BACKEND_URL!' | Set-Content traefik-dynamic.yml"
+echo BACKEND_URL=%BACKEND_URL%
 
-REM Запуск docker-compose
-docker-compose up --build
+REM Генерация traefik-dynamic.yml из шаблона
+powershell -Command ^
+  "(Get-Content traefik-dynamic.yml.template) -replace '\$\{BACKEND_URL\}', '%BACKEND_URL%' | Set-Content traefik-dynamic.yml"
+
+REM Build + Run
+docker compose build
+docker compose up -d
+
+pause
--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+set -e
 
-# Замена переменных в traefik-dynamic.yml перед запуском
+# Загружаем переменные из .env
+set -a
+source .env
+set +a
+
 export BACKEND_URL=${BACKEND_URL:-http://host.docker.internal:8000}
-envsubst '${BACKEND_URL}' < traefik-dynamic.yml.template > traefik-dynamic.yml
 
-docker-compose up --build
+echo "BACKEND_URL=$BACKEND_URL"
+
+envsubst '${BACKEND_URL}' \
+  < traefik-dynamic.yml.template \
+  > traefik-dynamic.yml
+
+docker compose up --build -d
